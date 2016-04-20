e26f3a893304e5e06c2ab8bdf5b90030edf56827
Michele <krazoafox89@gmail.com>
2016-04-20 20:45:01 +0930

    constants.js thankfully working again (Almost had a heart attack)
    I though it was just a one off lucky chance that it worked for a moment

--- constants.js
+++ constants.js
@@ -1,4 +1,16 @@
 //D = delta/displacement (change in somthing)
+//dimentions of a tile (in pixles)
+var TILE = 35;
+//width and high of a tile in the tileset
+var TILESET_TILE = TILE * 2;
+//pixles between the image boarder and the tile images in the tile map
+var TILESET_PADDING = 2;
+//spacing width between each tile in the tileset
+var TILESET_SPACING = 2;
+//how many coloumns of image tiles 
+var TILESET_COUNT_X = 14;
+//how many rows of image tiles
+var TILESET_COUNT_Y = 14;
 //setting distance mesurments
 var METRE = TILE; // every one tile equals 1m
 //setting gravity at which the player will fall at
@@ -21,15 +33,3 @@ var LAYER_PLATFORMS = 1; //1 = collision with a platform
 var LAYER_LADDERS = 2; //2 = collision with a ladder
 //level dimentions in tiles
 var MAP = {tw: 20, th: 15};
-//dimentions of a tile (in pixles)
-var TILE = 35;
-//width and high of a tile in the tileset
-var TILESET_TILE = TILE * 2;
-//pixles between the image boarder and the tile images in the tile map
-var TILESET_PADDING = 2;
-//spacing width between each tile in the tileset
-var TILESET_SPACING = 2;
-//how many coloumns of image tiles 
-var TILESET_COUNT_X = 14;
-//how many rows of image tiles
-var TILESET_COUNT_Y = 14;
